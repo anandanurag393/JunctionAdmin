52ba50940855b78e093f71cb0511e0141c31da02
Vivann <7vivann@gmail.com>
2020-02-28 23:30:10 +0530

    Booking

---
+++ app/src/main/java/com/example/JunctionAdmin/BookingQueue.java
@@ -0,0 +1,24 @@
+package com.example.JunctionAdmin;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class BookingQueue extends Fragment {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_booking_queue, container, false);
+    }
+}
